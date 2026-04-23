be4f0aebc33f521e79a8d59411daa01a1e9939db
Eric <eric@neovivify.com>
2026-04-22 20:07:15 -0400

    more database setup

--- user/userDatabase.go
+++ user/userDatabase.go
@@ -10,7 +10,6 @@ import (
 )
 
 type User struct {
-
 	Username string
 	PasswordHash string
 }
@@ -41,41 +40,41 @@ func InitalizeDatabase(dBName string) (Db, error) {
 
 	homeDir, err := os.UserHomeDir()
 	if err != nil {
-		return nil, err
+		return Db{}, err
 	}
 
 	dBAddress := filepath.Join(homeDir, fmt.Sprintf(".%s", dBName))
 
 	db, err := sql.Open("sqlite3", dBAddress)
 	if err != nil {
-		return nil, err
+		return Db{}, err
 	}
 
 	sqlStr := fmt.Sprintf(
 		`CREATE TABLE IF NOT EXISTS %s (
 			id TEXT NOT NULL PRIMARY KEY,
 			user TEXT NOT NULL,
-			passwordHASH BYTE NOT NULL,
+			passwordHash BYTE NOT NULL,
 			
 		);`, UserDB,
 		)
 
 	_, err = db.Exec(sqlStr)
 	if err != nil {
-		return nil, err
+		return Db{}, err
 	}
 
-	return Db(db), nil
+	return Db{db}, nil
 
 }
 
 // add the username to the usertable, store it with the salt and hashed password
-func AddUser(username string, databaseLocation string, passwordHash string) error {
-	db, err := sql.Open("sqlite3", databaseLocation)
-	if err != nil {
-		return err
-	}
-	defer db.Close()
+func AddUser(username string, db *Db, passwordHash string) error {
+
+	sqlStr := fmt.Sprintf(`INSERT INTO %s (id, user, passwordHash)
+		VALUES(?, ?, ?)`, UserDB)
+	
 	return nil
+	
 }
 
